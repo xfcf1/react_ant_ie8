ffe3bd33ffbe7117d49fa820a5810e33f060b8b2
xfcf1 <xfcf@163.com>
2016-05-23 19:11:38 +0800

    Update module.js

--- src/module.js
+++ src/module.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Router from 'react-router';
 import routes from 'routes';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-let history = createBrowserHistory();
+import createHashHistory from 'history/lib/createHashHistory';
+let history = createHashHistory({queryKey: false});
 
 import 'antd/style/index.less';
 
